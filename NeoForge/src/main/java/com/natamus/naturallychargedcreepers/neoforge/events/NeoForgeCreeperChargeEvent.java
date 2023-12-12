1efa4f85653a684f408a7fc4eaa2634b418d29e4
Rick South <rick@natamus.com>
2023-12-12 09:35:37 +0100

    Naturally Charged Creepers source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/naturallychargedcreepers/neoforge/events/NeoForgeCreeperChargeEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.naturallychargedcreepers.neoforge.events;
+
+import com.natamus.naturallychargedcreepers.events.CreeperChargeEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeCreeperChargeEvent {
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		CreeperChargeEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+}
