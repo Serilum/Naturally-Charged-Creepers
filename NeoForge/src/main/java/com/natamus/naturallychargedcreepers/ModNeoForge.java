81be12627ac008d3fd58aee04b76455dc6dc4dba
Rick South <rick@natamus.com>
2024-07-28 07:24:15 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/naturallychargedcreepers/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/naturallychargedcreepers/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.naturallychargedcreepers;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.naturallychargedcreepers.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.naturallychargedcreepers.neoforge.events.NeoForgeCreeperChargeEvent;
 import com.natamus.naturallychargedcreepers.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
