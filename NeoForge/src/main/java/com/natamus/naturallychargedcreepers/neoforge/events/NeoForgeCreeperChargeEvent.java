2a07618a7bbadb6dd337a7ea7a848cf671e8e0c3
Rick South <rick@natamus.com>
2025-02-12 23:24:28 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/naturallychargedcreepers/neoforge/events/NeoForgeCreeperChargeEvent.java
+++ NeoForge/src/main/java/com/natamus/naturallychargedcreepers/neoforge/events/NeoForgeCreeperChargeEvent.java
@@ -3,9 +3,7 @@ package com.natamus.naturallychargedcreepers.neoforge.events;
 import com.natamus.naturallychargedcreepers.events.CreeperChargeEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeCreeperChargeEvent {
 	@SubscribeEvent
 	public static void onEntityJoin(EntityJoinLevelEvent e) {
