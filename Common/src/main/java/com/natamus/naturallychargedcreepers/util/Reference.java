638a58cd4812abd97e2dc8d98eb509d0e4b52db3
Rick South <rick@natamus.com>
2026-06-18 23:38:29 +0200

    Updated Naturally Charged Creepers to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/naturallychargedcreepers/util/Reference.java
+++ Common/src/main/java/com/natamus/naturallychargedcreepers/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "naturallychargedcreepers";
 	public static final String NAME = "Naturally Charged Creepers";
 	public static final String VERSION = "3.6";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
